929a3288b9ecf0558c514d24c112247f01b02d72
markthree <1801982702@qq.com>
2022-01-29 23:45:39 +0800

    feat(utilities): syncRef

---
+++ uni_modules/tob-use/utilities/syncRef/README.md
@@ -0,0 +1,13 @@
+# syncRef
+
+保持目标ref跟源同步
+
+## Usage
+
+```js
+import { ref } from 'vue'
+import { syncRef } from '@/uni_modules/tob-use/index.js'
+
+
+```
+
---
+++ uni_modules/tob-use/utilities/syncRef/index.js
@@ -0,0 +1,23 @@
+import { watch } from 'vue'
+
+/**
+ * 保持目标ref跟源同步
+ */
+export const syncRef = (source, targets, options = {}) => {
+	const {
+		deep = false,
+		flush = 'sync',
+		immediate = true
+	} = options
+
+	if (!Array.isArray(targets)) {
+		targets = [targets]
+	}
+
+	return watch(
+		source,
+		newValue =>
+			targets.forEach(target => (target.value = newValue)),
+		{ flush, deep, immediate }
+	)
+}
