49fc5b5e1bbeba24a2f8ee53e1d6ab920e8961a8
markthree <1801982702@qq.com>
2022-02-16 19:16:38 +0800

    review(utilities): set

--- uni_modules/tob-use/utilities/set/index.js
+++ uni_modules/tob-use/utilities/set/index.js
@@ -2,10 +2,13 @@
  * 设置
  */
 export const set = (...args) => {
+	// 双参数时，直接赋值
 	if (args.length === 2) {
 		const [ref, value] = args
 		ref.value = value
 	}
+
+	// 三参数时，赋值到某个键
 	if (args.length === 3) {
 		const [target, key, value] = args
 		target[key] = value
