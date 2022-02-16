a98bb011dc99975eeb9387347519f65a649e7107
markthree <1801982702@qq.com>
2022-02-16 20:32:54 +0800

    review(utilities): useCounter

--- uni_modules/tob-use/utilities/useCounter/index.js
+++ uni_modules/tob-use/utilities/useCounter/index.js
@@ -3,13 +3,18 @@ import { ref } from 'vue'
 /**
  * 计数器
  */
-export const useCounter = () => {
+export const useCounter = (initialValue = 0) => {
 	const count = ref(initialValue)
 
+	// 递增
 	const inc = (delta = 1) => (count.value += delta)
+	// 递减
 	const dec = (delta = 1) => (count.value -= delta)
+	// 获取
 	const get = () => count.value
+	// 设置
 	const set = val => (count.value = val)
+	// 重置
 	const reset = (val = initialValue) => {
 		initialValue = val
 		return set(val)
