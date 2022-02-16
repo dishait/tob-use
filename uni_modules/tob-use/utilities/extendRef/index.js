b252c59a18fed6fade66b5c7d6fd4cec52b19566
markthree <1801982702@qq.com>
2022-02-16 18:11:31 +0800

    review(utilities): extendRef

--- uni_modules/tob-use/utilities/extendRef/index.js
+++ uni_modules/tob-use/utilities/extendRef/index.js
@@ -1,13 +1,15 @@
 import { isRef } from 'vue'
 
 /**
- * ref扩展
+ * ref 扩展
  */
 export const extendRef = (ref, extend, options = {}) => {
 	const { enumerable = false, unwrap = true } = options
 
 	for (const [key, value] of Object.entries(extend)) {
-		if (key === 'value') continue
+		if (key === 'value') continue // 忽略 .value 键
+
+		// 保留原有 ref
 		if (isRef(value) && unwrap) {
 			Object.defineProperty(ref, key, {
 				get() {
