3f60b2a4523a6f75fd2354f06dfcffedbb6f9153
markthree <1801982702@qq.com>
2022-02-16 18:12:29 +0800

    review(utilities): createEventHook

--- uni_modules/tob-use/utilities/createEventHook/index.js
+++ uni_modules/tob-use/utilities/createEventHook/index.js
@@ -1,14 +1,16 @@
 /**
- * 创建事件hook
+ * 创建事件 hook
  */
 export const createEventHook = () => {
 	const fns = []
 
+	// 卸载
 	const off = fn => {
 		const index = fns.indexOf(fn)
 		if (index !== -1) fns.splice(index, 1)
 	}
 
+	// 注册
 	const on = fn => {
 		fns.push(fn)
 		return {
@@ -16,6 +18,7 @@ export const createEventHook = () => {
 		}
 	}
 
+	// 触发
 	const trigger = param => {
 		fns.forEach(fn => fn(param))
 	}
