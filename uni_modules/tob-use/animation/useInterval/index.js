1cba7dc9e8eae32dca6664d9d8d9ab1365f4b86a
markthree <1801982702@qq.com>
2022-02-16 13:53:53 +0800

    feat(misc): useTimeAgo

---
+++ uni_modules/tob-use/animation/useInterval/index.js
@@ -0,0 +1,30 @@
+import { ref, computed, watch } from 'vue'
+
+/**
+ * 间隔
+ */
+export const useInterval = (
+	interval = 1000,
+	options = {}
+) => {
+	const {
+		controls: exposeControls = false,
+		immediate = true
+	} = options
+
+	const counter = ref(0)
+	const controls = useIntervalFn(
+		() => (counter.value += 1),
+		interval,
+		{ immediate }
+	)
+
+	if (exposeControls) {
+		return {
+			counter,
+			...controls
+		}
+	} else {
+		return counter
+	}
+}
---
+++ uni_modules/tob-use/misc/useTimeAgo/README.md
@@ -0,0 +1,13 @@
+# useTimeAgo
+
+时间线
+
+## Usage
+
+```js
+import { ref } from 'vue'
+import { useTimeAgo } from '@/uni_modules/tob-use/index.js'
+
+
+```
+
