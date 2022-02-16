01e7392f818b970933446abb590cc539c8e04fb1
markthree <1801982702@qq.com>
2022-02-16 19:12:16 +0800

    review(utilities): reactivePick

--- uni_modules/tob-use/utilities/reactivePick/index.js
+++ uni_modules/tob-use/utilities/reactivePick/index.js
@@ -3,7 +3,7 @@ import { toRef, reactive } from 'vue'
 /**
  * 响应式的pick
  */
-export const reactivePick = () => {
+export const reactivePick = (obj, ...keys) => {
 	return reactive(
 		Object.fromEntries(keys.map(k => [k, toRef(obj, k)]))
 	)
