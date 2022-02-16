5960828f054e96577ac235beb49087164282fab2
markthree <1801982702@qq.com>
2022-02-16 18:16:30 +0800

    review(utilities): reactify

--- uni_modules/tob-use/utilities/createReactiveFn/index.js
+++ uni_modules/tob-use/utilities/createReactiveFn/index.js
@@ -1,15 +1,1 @@
-import { computed, unref } from 'vue'
-
-/**
- * 创建响应式函数
- */
-export const createReactiveFn = () => {
-	return function (this, ...args) {
-		return computed(() =>
-			fn.apply(
-				this,
-				args.map(v => unref(v))
-			)
-		)
-	}
-}
+export { reactify as createReactiveFn } from '../reactify'
--- uni_modules/tob-use/utilities/reactify/index.js
+++ uni_modules/tob-use/utilities/reactify/index.js
@@ -4,7 +4,13 @@ import { computed, unref } from 'vue'
  * 响应式转换
  */
 export const reactify = () => {
-    return function(this, ...args) {
-        return computed(() => fn.apply(this, args.map(i => unref(i))))
-    }
+	return function (...args) {
+		return computed(() =>
+			fn.apply(
+				this,
+				// 解除所有 ref 的参数
+				args.map(v => unref(v))
+			)
+		)
+	}
 }
