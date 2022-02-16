964bbf138399319ac3f6678675c701d1fca4ff58
markthree <1801982702@qq.com>
2022-02-16 14:41:34 +0800

    feat(component): tryOnMounted

---
+++ uni_modules/tob-use/component/tryOnMounted/index.js
@@ -0,0 +1,17 @@
+import {
+	getCurrentInstance,
+	nextTick,
+	onMounted
+} from 'vue'
+/**
+ * 在挂载后触发
+ */
+export const tryOnMounted = (fn, sync = true) => {
+	if (getCurrentInstance()) {
+		onMounted(fn)
+	} else if (sync) {
+		fn()
+	} else {
+		nextTick(fn)
+	}
+}
