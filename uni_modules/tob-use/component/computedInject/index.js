0827d9e35213ae2573cb00cdf0c939460055fd56
markthree <1801982702@qq.com>
2022-02-17 14:24:08 +0800

    review(component): computedInject

--- uni_modules/tob-use/component/computedInject/index.js
+++ uni_modules/tob-use/component/computedInject/index.js
@@ -4,10 +4,16 @@ import { isFunction } from '../../shared/is'
 /**
  * 计算属性型 inject
  */
-export const computedInject = () => {
+export const computedInject = (
+	key,
+	options,
+	defaultSource,
+	treatDefaultAsFactory
+) => {
 	let source = inject(key)
 
 	if (defaultSource) source = inject(key, defaultSource)
+	// 使得默认值作为工厂函数
 	if (treatDefaultAsFactory)
 		source = inject(
 			key,
